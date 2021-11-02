9f5d5019b4f3d6535c9996092ea5681cafc50ef0
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-02 20:19:56 +0100

    Create file Rocket.java

---
+++ src/Rocket.java
@@ -0,0 +1,22 @@
+public class Rocket implements SpaceShip {
+
+    @Override
+    public boolean launch() {
+        return false;
+    }
+
+    @Override
+    public boolean land() {
+        return false;
+    }
+
+    @Override
+    public boolean canCarry(Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean carry(Item item) {
+        return false;
+    }
+}
