ee0eec91c47117721d65eff9cf1c89d5381a4efc
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-04 21:29:28 +0100

    Update add code U1.java

--- src/U1.java
+++ src/U1.java
@@ -1,6 +1,24 @@
 public class U1 extends Rocket {
-
     private static int rocketCountU1;
+    private static final int cost = 100000000;
+    private final int weight = 10000;
+    private final int maxWeight = 8000;
+    private double launchExp;
+    private double landCrash;
+
+    public U1() {
+        this(0);
+    }
+
+    public U1(int lastWght) {
+        setMaxWeight(maxWeight);
+        setCost(cost);
+        setWeight(weight);
+        setCurWeight(lastWght);
+        rocketCountU1++;
+        System.out.println("___________________________");
+    }
+
     public static int getRocketCountU1() {
         return rocketCountU1;
     }
