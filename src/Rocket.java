7064f16def661169420a895284ddc001e0f27bec
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-04 20:07:51 +0100

    Change and add code to the class in file (Rocket & SpaceShip).java

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,16 +1,12 @@
 public class Rocket implements SpaceShip {
-    int cost;
-    int weight;
-    int maxWeight;
-    int curWeight;
-    double launchExp;
-    double landCrash;
+    public int cost;
+    public int weight;
+    public int maxWeight;
+    public int curWeight;
+    public double totalWgt = curWeight + weight;
 
     @Override
-    public boolean launch() {
-        return true;
-    }
-
+    public boolean launch() { return true; }
     @Override
     public boolean land() {
         return true;
@@ -18,11 +14,49 @@ public class Rocket implements SpaceShip {
 
     @Override
     public boolean canCarry(Item item) {
+        if(maxWeight >= (curWeight + item.getWeight()))
+            return true;
+        else
+            System.out.println("You cannot wear an item : " + item.getName() + "\tWeight : " + item.getWeight());
         return false;
     }
 
     @Override
-    public boolean carry(Item item) {
-        return false;
+    public void carry(Item item) {
+        System.out.println("Carrying Item : " + item.getName() + "\tWeight : " + item.getWeight());
+        curWeight += item.getWeight();
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getTotalWgt() {
+        totalWgt = getCurWeight() + getWeight();
+        return totalWgt;
+    }
+
+    public int getCurWeight() {
+        return curWeight;
+    }
+
+    public void setCurWeight(int curWeight) {
+        this.curWeight = curWeight;
     }
 }
