cb63691769ec022a4ee546feb117141e9b866e78
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-05 19:57:50 +0100

    Add code in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    public static long totalCost(int i) {
+        return (i == 1) ? U1.getTotalCost() : U2.getTotalCost();
+    }
+    
 }
