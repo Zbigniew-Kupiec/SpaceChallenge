fe7957b6f97674f9ce8fd5c0f09fc1093cbf0326
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-05 18:41:18 +0100

    Update U1.java

--- src/U1.java
+++ src/U1.java
@@ -1,18 +1,18 @@
+import java.util.Random;
+
 public class U1 extends Rocket {
     private static int rocketCountU1;
     private static final int cost = 100000000;
-    private final int weight = 10000;
-    private final int maxWeight = 8000;
-    private double launchExp;
-    private double landCrash;
 
     public U1() {
         this(0);
     }
 
     public U1(int lastWght) {
+        int maxWeight = 8000;
         setMaxWeight(maxWeight);
         setCost(cost);
+        int weight = 10000;
         setWeight(weight);
         setCurWeight(lastWght);
         rocketCountU1++;
@@ -22,8 +22,39 @@ public class U1 extends Rocket {
     public static int getRocketCountU1() {
         return rocketCountU1;
     }
-
     public static void setRocketCountU1(int rocketCountU1) {
         U1.rocketCountU1 = rocketCountU1;
+    }
+    @Override
+    public boolean launch() {
+        System.out.println("\n " + " ^^^ LAUNCH ^^^");
+        double randomNr = new Random().nextDouble();
+        // double random = randomNr.nextDouble();
+        double launchExp = 0.05 * (getTotalWgt() / getMaxWeight());
+        System.out.println("CARGO : " + getCurWeight());
+        if (launchExp >= randomNr) {
+            System.out.println("EXPLODE U1 !!! " + "\n" + "... SEND NEW ROCKET, BECAUSE U1 EXPLODED ... ");
+            return false;
+        } else {
+            System.out.println("SUCCESSFUL START U1");
+            return true;
+        }
+    }
+    @Override
+    public boolean land() {
+        System.out.println("\n" + "___ LANDING ___");
+        double randomNr = new Random().nextDouble();
+        // double random = randomNr.nextDouble();
+        double landCrash = 0.01 * (getTotalWgt() / getMaxWeight());
+        if (landCrash >= randomNr) {
+            System.out.println("CRASHED U1 !!! " + "\n" +  "... SEND NEW ROCKET, BECAUSE U1 EXPLODED ... ");
+            return false;
+        } else {
+            System.out.println("SUCCESSFUL LAND U1");
+            return true;
+        }
+    }
+    public static long getTotalCost() {
+        return (long) getRocketCountU1() * cost;
     }
 }
