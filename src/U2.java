7567a14c9aa91ac2858bd0218d33b3d22b02031d
Zbigniew Kupiec <zk-design@outlook.com>
2021-11-05 19:49:06 +0100

    Update U2.java

--- src/U2.java
+++ src/U2.java
@@ -1,10 +1,58 @@
+import java.util.Random;
+
 public class U2 extends Rocket {
     private static int rocketCountU2;
+    private static final int cost = 120000000;
+
+    public U2() {
+        this(0);
+    }
+    public U2(int lastWght) {
+        int maxWeight = 11000;
+        setMaxWeight(maxWeight);
+        setCost(cost);
+        int weight = 18000;
+        setWeight(weight);
+        setCurWeight(lastWght);
+        rocketCountU2++;
+        System.out.println("_____________________________");
+    }
     public static int getRocketCountU2() {
         return rocketCountU2;
     }
-
     public static void setRocketCountU2(int rocketCountU2) {
         U2.rocketCountU2 = rocketCountU2;
+    }
+    @Override
+    public boolean launch() {
+        System.out.println("\n " + " ^^^ LAUNCH ^^^");
+        double randomNr = new Random().nextDouble();
+        // double random = randomNr.nextDouble();
+        double launchExp = 0.04 * (getTotalWgt() / getMaxWeight());
+        System.out.println("CARGO : " + getCurWeight());
+        if (launchExp >= randomNr) {
+            System.out.println("EXPLODE U2 !!! " + "\n" + "... SEND NEW ROCKET, BECAUSE U2 EXPLODED ... ");
+            return false;
+        } else {
+            System.out.println("SUCCESSFUL START U2");
+            return true;
+        }
+    }
+    @Override
+    public boolean land() {
+        System.out.println("\n" + "___ LANDING ___");
+        double randomNr = new Random().nextDouble();
+        // double random = randomNr.nextDouble();
+        double landCrash = 0.02 * (getTotalWgt() / getMaxWeight());
+        if (landCrash >= randomNr) {
+            System.out.println("CRASHED U2 !!! " + "\n" +  "... SEND NEW ROCKET, BECAUSE U2 EXPLODED ... ");
+            return false;
+        } else {
+            System.out.println("SUCCESSFUL LAND U2");
+            return true;
+        }
+    }
+    public static long getTotalCost() {
+        return (long) getRocketCountU2() * cost;
     }
 }
